3045f4dadde8eaccc71796529403c4e66afdd9aa
Johlert <53093811+Johlert@users.noreply.github.com>
2022-02-19 23:43:25 +0100

    Added V-Sync and Title

--- desktop/src/com/pokemon/desktop/DesktopLauncher.java
+++ desktop/src/com/pokemon/desktop/DesktopLauncher.java
@@ -1,5 +1,7 @@
 package com.pokemon.desktop;
 
+import com.badlogic.gdx.Files;
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
 import com.pokemon.Pokemon;
@@ -7,7 +9,9 @@ import com.pokemon.Pokemon;
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		config.title = "Pokemon";
 		config.forceExit = false;
+		config.vSyncEnabled = true;
 		new LwjglApplication(new Pokemon(), config);
 	}
 }
