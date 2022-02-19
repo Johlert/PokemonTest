c9dae828a16add441e48ea53fed7602caf2cc7c7
Johlert <53093811+Johlert@users.noreply.github.com>
2022-02-19 23:44:13 +0100

    Create MainMenu.java

---
+++ core/src/com/pokemon/screens/MainMenu.java
@@ -0,0 +1,51 @@
+package com.pokemon.screens;
+
+import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.ui.Table;
+import com.pokemon.Pokemon;
+
+public class MainMenu implements Screen {
+    private Pokemon pokemon;
+    private Stage stage;
+    private Table table;
+
+    public MainMenu(Pokemon pokemon) {
+        this.pokemon = pokemon;
+    }
+
+    @Override
+    public void show() {
+
+    }
+
+    @Override
+    public void render(float delta) {
+
+    }
+
+    @Override
+    public void resize(int width, int height) {
+
+    }
+
+    @Override
+    public void pause() {
+
+    }
+
+    @Override
+    public void resume() {
+
+    }
+
+    @Override
+    public void hide() {
+
+    }
+
+    @Override
+    public void dispose() {
+
+    }
+}
