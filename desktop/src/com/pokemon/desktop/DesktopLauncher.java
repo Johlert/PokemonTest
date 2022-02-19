5da193cbd0b55d8bf61a11a4ab74a13dde4459d0
Johlert <53093811+Johlert@users.noreply.github.com>
2022-02-19 23:58:09 +0100

    Fixed imports

--- desktop/src/com/pokemon/desktop/DesktopLauncher.java
+++ desktop/src/com/pokemon/desktop/DesktopLauncher.java
@@ -1,7 +1,5 @@
 package com.pokemon.desktop;
 
-import com.badlogic.gdx.Files;
-import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
 import com.pokemon.Pokemon;
