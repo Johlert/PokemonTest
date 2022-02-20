d25892e0074f3fabc71e428d28443d89e0036f54
Johlert <53093811+Johlert@users.noreply.github.com>
2022-02-20 16:58:29 +0100

    Update MainMenu.java

--- core/src/com/pokemon/screens/MainMenu.java
+++ core/src/com/pokemon/screens/MainMenu.java
@@ -1,27 +1,119 @@
 package com.pokemon.screens;
 
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
+import com.badlogic.gdx.graphics.g2d.TextureAtlas;
+import com.badlogic.gdx.scenes.scene2d.InputEvent;
 import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.ui.Label;
+import com.badlogic.gdx.scenes.scene2d.ui.Skin;
 import com.badlogic.gdx.scenes.scene2d.ui.Table;
+import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
+import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
 import com.pokemon.Pokemon;
 
 public class MainMenu implements Screen {
-    private Pokemon pokemon;
-    private Stage stage;
-    private Table table;
+    private final Pokemon pokemon;
+    private final Stage stage;
+    private final TextureAtlas atlas;
+    private final TextButton.TextButtonStyle buttonStyle;
+    private final Label.LabelStyle labelStyle;
+    private final Skin skin;
+    private final Table table;
+    private final TextButton hostButton;
+    private final TextButton joinButton;
+    private final TextButton settingsButton;
+    private final TextButton quitButton;
+    private final BitmapFont white;
+    private final BitmapFont black;
+    private final Label heading;
 
     public MainMenu(Pokemon pokemon) {
         this.pokemon = pokemon;
+        stage = new Stage();
+        atlas = new TextureAtlas("atlas/atlas.pack");
+        buttonStyle = new TextButton.TextButtonStyle();
+        labelStyle = new Label.LabelStyle();
+        skin = new Skin(atlas);
+        table = new Table();
+        white = new BitmapFont(Gdx.files.internal("fonts/white.fnt"), false);
+        black = new BitmapFont(Gdx.files.internal("fonts/black.fnt"), false);
+
+        buttonStyle.up = skin.getDrawable("button.up");
+        buttonStyle.down = skin.getDrawable("button.down");
+        buttonStyle.pressedOffsetX = 1;
+        buttonStyle.pressedOffsetY = -1;
+        buttonStyle.font = black;
+
+        labelStyle.font = white;
+        labelStyle.fontColor = Color.WHITE;
+
+        heading = new Label("POKEMON", labelStyle);
+
+        hostButton = new TextButton("HOST", buttonStyle);
+        joinButton = new TextButton("JOIN", buttonStyle);
+        settingsButton = new TextButton("SETTINGS", buttonStyle);
+        quitButton = new TextButton("QUIT", buttonStyle);
+
+        hostButton.pad(8);
+        joinButton.pad(8);
+        settingsButton.pad(8);
+        quitButton.pad(8);
+
+        hostButton.addListener(new ClickListener() {
+            @Override
+            public void clicked(InputEvent event, float x, float y) {
+
+            }
+        });
+        joinButton.addListener(new ClickListener() {
+            @Override
+            public void clicked(InputEvent event, float x, float y) {
+
+            }
+        });
+        settingsButton.addListener(new ClickListener() {
+            @Override
+            public void clicked(InputEvent event, float x, float y) {
+
+            }
+        });
+        quitButton.addListener(new ClickListener() {
+            @Override
+            public void clicked(InputEvent event, float x, float y) {
+                Gdx.app.exit();
+            }
+        });
     }
 
     @Override
     public void show() {
+        Gdx.input.setInputProcessor(stage);
 
+        table.setBounds(0, 0, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
+        table.add(heading);
+        table.getCell(heading).spaceBottom(90);
+        table.row();
+        table.add(hostButton);
+        table.getCell(hostButton).spaceBottom(5);
+        table.row();
+        table.add(joinButton);
+        table.getCell(joinButton).spaceBottom(5);
+        table.row();
+        table.add(settingsButton);
+        table.getCell(settingsButton).spaceBottom(5);
+        table.row();
+        table.add(quitButton);
+
+        stage.addActor(table);
     }
 
     @Override
     public void render(float delta) {
-
+        stage.act(delta);
+        stage.draw();
     }
 
     @Override
@@ -46,6 +138,10 @@ public class MainMenu implements Screen {
 
     @Override
     public void dispose() {
-
+        stage.dispose();
+        atlas.dispose();
+        skin.dispose();
+        white.dispose();
+        black.dispose();
     }
 }
