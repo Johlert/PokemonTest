0e05d1921ee37fe66f2444c4f5c882b542bbbbe5
Johlert <53093811+Johlert@users.noreply.github.com>
2022-02-21 16:42:48 +0100

    Update Pokemon.java

--- core/src/com/pokemon/view/Pokemon.java
+++ core/src/com/pokemon/view/Pokemon.java
@@ -5,6 +5,7 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.pokemon.view.screens.game.GameScreen;
+import com.pokemon.view.screens.menu.MainMenuScreen;
 
 public class Pokemon extends Game {
     private SpriteBatch batch;
@@ -16,7 +17,7 @@ public class Pokemon extends Game {
     @Override
     public void create() {
         batch = new SpriteBatch();
-        setScreen(new GameScreen(this));
+        setScreen(new MainMenuScreen(this));
     }
 
     @Override
