ad1461ddaf75145a945fbf343541ce1c05d00c7e
Johlert <53093811+Johlert@users.noreply.github.com>
2022-02-21 16:42:08 +0100

    Create Ability.java

---
+++ core/src/com/pokemon/model/Pokemon/Ability.java
@@ -0,0 +1,15 @@
+package com.pokemon.model.Pokemon;
+
+import lombok.Data;
+
+public @Data class Ability {
+
+    private int id;
+    private String name;
+    private int power;
+    private int accuracy;
+    private Type type;
+    private boolean isSpecial;
+
+
+}
