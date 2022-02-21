2f09ebc957013ef9cecdb538bdaf5f6e4ce6388b
Johlert <53093811+Johlert@users.noreply.github.com>
2022-02-21 16:41:30 +0100

    Create ItemStack.java

---
+++ core/src/com/pokemon/model/Items/ItemStack.java
@@ -0,0 +1,28 @@
+package com.pokemon.model.Items;
+
+public abstract class ItemStack {
+
+    private int amount;
+    private Item item;
+
+    public ItemStack(int amount, Item item) {
+        this.amount = amount;
+        this.item = item;
+    }
+
+    public int getAmount() {
+        return amount;
+    }
+
+    public void setAmount(int amount) {
+        this.amount = amount;
+    }
+
+    public Item getItem() {
+        return item;
+    }
+
+    public void setItem(Item item) {
+        this.item = item;
+    }
+}
