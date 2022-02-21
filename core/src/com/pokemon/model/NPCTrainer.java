5666b359055cada81d6444774fe42c8bfa453b56
Johlert <53093811+Johlert@users.noreply.github.com>
2022-02-21 16:41:55 +0100

    Create NPCTrainer.java

---
+++ core/src/com/pokemon/model/NPCTrainer.java
@@ -0,0 +1,15 @@
+package com.pokemon.model;
+
+
+import com.pokemon.model.Pokemon.Pokemon;
+import com.pokemon.model.Pokemon.Trainer;
+
+public class NPCTrainer extends NPC implements Trainer {
+
+    //todo
+
+    @Override
+    public Pokemon getActivePokemon() {
+        return null;
+    }
+}
