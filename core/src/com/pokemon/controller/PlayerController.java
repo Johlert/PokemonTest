d17ce8d48b71610343c5c342c6780c5d74d4e39e
Johlert <53093811+Johlert@users.noreply.github.com>
2022-02-21 16:40:48 +0100

    Update PlayerController.java

--- core/src/com/pokemon/controller/PlayerController.java
+++ core/src/com/pokemon/controller/PlayerController.java
@@ -2,8 +2,12 @@ package com.pokemon.controller;
 
 import com.badlogic.gdx.Input;
 import com.badlogic.gdx.InputAdapter;
+import com.pokemon.model.CacheForPoke;
+import com.pokemon.model.Direction;
+import com.pokemon.model.Events.EventQueue;
+import com.pokemon.model.Events.MoveEvent;
 import com.pokemon.model.Player;
-import com.pokemon.model.Settings;
+import com.pokemon.model.Global;
 
 public class PlayerController extends InputAdapter {
     private Player player;
@@ -14,20 +18,33 @@ public class PlayerController extends InputAdapter {
 
     @Override
     public boolean keyDown(int keycode) {
-        if (keycode == Input.Keys.W){
-            player.move(0, Settings.TILE_SIZE);
+
+        if (keycode == CacheForPoke.getInstance().getLocalP().getUserSettings().getMoveForward()){
+            MoveEvent mv = new MoveEvent(CacheForPoke.getInstance().getLocalP(), Direction.UP);
+            CacheForPoke.getInstance().getPostOffice().broadcast(mv);
+            EventQueue.getINSTANCE().addEvent(mv);
+            //player.move(0, Global.TILE_SIZE);
         }
 
-        if (keycode == Input.Keys.S){
-            player.move(0, -Settings.TILE_SIZE);
+        if (keycode == CacheForPoke.getInstance().getLocalP().getUserSettings().getMoveDown()){
+            MoveEvent mv = new MoveEvent(CacheForPoke.getInstance().getLocalP(), Direction.DOWN);
+            CacheForPoke.getInstance().getPostOffice().broadcast(mv);
+            EventQueue.getINSTANCE().addEvent(mv);
+            //player.move(0, -Global.TILE_SIZE);
         }
 
-        if (keycode == Input.Keys.A){
-            player.move(-Settings.TILE_SIZE, 0);
+        if (keycode == CacheForPoke.getInstance().getLocalP().getUserSettings().getMoveLeft()){
+            MoveEvent mv = new MoveEvent(CacheForPoke.getInstance().getLocalP(), Direction.LEFT);
+            CacheForPoke.getInstance().getPostOffice().broadcast(mv);
+            EventQueue.getINSTANCE().addEvent(mv);
+            //player.move(-Global.TILE_SIZE, 0);
         }
 
-        if (keycode == Input.Keys.D){
-            player.move(Settings.TILE_SIZE, 0);
+        if (keycode == CacheForPoke.getInstance().getLocalP().getUserSettings().getMoveRight()){
+            MoveEvent mv = new MoveEvent(CacheForPoke.getInstance().getLocalP(), Direction.RIGHT);
+            CacheForPoke.getInstance().getPostOffice().broadcast(mv);
+            EventQueue.getINSTANCE().addEvent(mv);
+            //player.move(Global.TILE_SIZE, 0);
         }
 
         return false;
