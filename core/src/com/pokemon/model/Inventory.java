785e224c0cca5b83051242844e698aa3e3bd73f1
Johlert <53093811+Johlert@users.noreply.github.com>
2022-02-21 16:41:23 +0100

    Create Inventory.java

---
+++ core/src/com/pokemon/model/Inventory.java
@@ -0,0 +1,17 @@
+package com.pokemon.model;
+
+import com.pokemon.model.Items.ItemStack;
+
+import java.util.LinkedList;
+
+public class Inventory {
+
+    private int money;
+
+    private LinkedList<ItemStack> inventory = new LinkedList<>();
+
+    public Inventory(){
+        
+    }
+    //todo
+}
