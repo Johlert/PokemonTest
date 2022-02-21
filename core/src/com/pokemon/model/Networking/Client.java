42be82bcf253f242192e1fc05b16c82c81b6cfb5
Johlert <53093811+Johlert@users.noreply.github.com>
2022-02-21 16:41:41 +0100

    Create Client.java

---
+++ core/src/com/pokemon/model/Networking/Client.java
@@ -0,0 +1,82 @@
+package com.pokemon.model.Networking;
+
+import com.badlogic.gdx.Net;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.badlogic.gdx.backends.lwjgl.LwjglNet;
+import com.badlogic.gdx.net.Socket;
+import com.badlogic.gdx.net.SocketHints;
+import com.pokemon.model.CacheForPoke;
+import com.pokemon.model.Events.Event;
+import com.pokemon.model.Events.EventQueue;
+import com.pokemon.model.Player;
+import lombok.SneakyThrows;
+
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.io.Serializable;
+
+public class Client implements PostOffice {
+
+
+    Socket socket;
+    Net net = new LwjglNet(new LwjglApplicationConfiguration());
+
+    public boolean connect(String ip, String playername) {
+        socket = net.newClientSocket(Net.Protocol.TCP, ip, 5000, new SocketHints());
+        if(socket.isConnected()){
+            new Listener(socket, playername);
+        }
+        return socket.isConnected();
+    }
+
+    /**
+     * All fields in s must be Serializable and all fields of the fields in s must be Serializable
+     *
+     * @param s
+     * @throws Exception
+     */
+    public void send(Serializable s){
+        try {
+            ObjectOutputStream objectOutputStream = new ObjectOutputStream(socket.getOutputStream());
+            objectOutputStream.writeObject(s);
+            objectOutputStream.flush();
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+
+    public class Listener {
+        @SneakyThrows
+        public Listener(Socket s, String localPlayer) {
+            socket = s;
+            send(localPlayer);
+            final ObjectInputStream objectInputStream = new ObjectInputStream(s.getInputStream());
+            Player player = (Player) objectInputStream.readObject();
+            CacheForPoke.getInstance().setLocalP(player);
+            new Thread(new Runnable() {
+                @Override
+                public void run() {
+                    try {
+
+
+                        while (isListening) {
+                            Event event = (Event) objectInputStream.readObject();
+                            EventQueue.getINSTANCE().addEvent(event);
+                        }
+                        objectInputStream.close();
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+                }
+            }).start();
+        }
+
+        boolean isListening = true;
+        Socket socket;
+    }
+
+    @Override
+    public void broadcast(Event e) {
+        send(e);
+    }
+}
