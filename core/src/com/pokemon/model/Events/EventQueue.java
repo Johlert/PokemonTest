f8667d4db1e5fe9d9ba5b38606900f5eed3066d0
Johlert <53093811+Johlert@users.noreply.github.com>
2022-02-21 16:41:04 +0100

    Create EventQueue.java

---
+++ core/src/com/pokemon/model/Events/EventQueue.java
@@ -0,0 +1,32 @@
+package com.pokemon.model.Events;
+
+import java.util.LinkedList;
+
+public class EventQueue {
+
+    private static EventQueue INSTANCE;
+    private LinkedList<Event> events = new LinkedList<>();
+
+    private EventQueue(){
+    }
+
+    public static EventQueue getINSTANCE() {
+        if(INSTANCE == null){
+            INSTANCE = new EventQueue();
+        }
+        return INSTANCE;
+    }
+
+    public void addEvent(Event e){
+        events.add(e);
+    }
+
+    public Event popEvent(){
+        return events.pop();
+    }
+
+    public LinkedList<Event> getEvents() {
+        return events;
+    }
+
+}
