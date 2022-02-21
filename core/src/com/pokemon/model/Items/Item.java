2dd82e5b8a20b7ed03819f2c0ed15d343ba8521d
Johlert <53093811+Johlert@users.noreply.github.com>
2022-02-21 16:41:26 +0100

    Create Item.java

---
+++ core/src/com/pokemon/model/Items/Item.java
@@ -0,0 +1,38 @@
+package com.pokemon.model.Items;
+
+import com.badlogic.gdx.graphics.Texture;
+
+public abstract class Item {
+
+    //items are consumeble items
+
+    private int id;
+    private String name;
+    private String description;
+
+    public Item(String name, String description, int id) {
+        this.name = name;
+        this.description = description;
+        this.id = id;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+}
