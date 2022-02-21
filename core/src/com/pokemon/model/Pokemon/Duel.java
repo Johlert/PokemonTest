70d4b71a9bb44d945c793da1060af80c9fe57458
Johlert <53093811+Johlert@users.noreply.github.com>
2022-02-21 16:42:11 +0100

    Create Duel.java

---
+++ core/src/com/pokemon/model/Pokemon/Duel.java
@@ -0,0 +1,33 @@
+package com.pokemon.model.Pokemon;
+
+public class Duel {
+
+    private Trainer trainer1;
+    private Ability trainer1Move;
+    private boolean hasP1Moved = false;
+
+
+
+    private Trainer trainer2;
+    private Ability trainer2Move;
+    private boolean hasP2Moved = false;
+
+    public void setTrainer1Move(Ability trainer1Move) {
+        this.trainer1Move = trainer1Move;
+        hasP1Moved = true;
+    }
+
+    public void setTrainer2Move(Ability trainer2Move) {
+        this.trainer2Move = trainer2Move;
+        hasP2Moved = true;
+    }
+
+    private void resolveRound(){
+        if(hasP1Moved && hasP2Moved){
+
+            //todo fighting
+
+
+        }
+    }
+}
