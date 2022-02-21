cbc480402055064286529dc3fb16ba93315e2684
Johlert <53093811+Johlert@users.noreply.github.com>
2022-02-21 16:41:33 +0100

    Create PokeBall.java

---
+++ core/src/com/pokemon/model/Items/PokeBalls/PokeBall.java
@@ -0,0 +1,27 @@
+package com.pokemon.model.Items.PokeBalls;
+
+import com.badlogic.gdx.graphics.Texture;
+import com.pokemon.model.Items.Item;
+import lombok.Getter;
+import lombok.Setter;
+
+public @Getter @Setter class PokeBall extends Item {
+
+
+    //pokeballs are used only in combat. They are 1 time use items
+    private int captureModifier;
+
+    private Texture texture;
+
+    public PokeBall(String name, String description, int captureModifier, Texture texture, int id) {
+        super(name, description, id);
+        this.texture = texture;
+
+
+        this.captureModifier = captureModifier;
+    }
+
+    public int getCaptureModifier() {
+        return captureModifier;
+    }
+}
