045e2e16d601c6138682e022c60dbf9bc9b951a4
Johlert <53093811+Johlert@users.noreply.github.com>
2022-02-21 16:41:02 +0100

    Create EventHandler.java

---
+++ core/src/com/pokemon/model/Events/EventHandler.java
@@ -0,0 +1,38 @@
+package com.pokemon.model.Events;
+
+import java.util.LinkedList;
+
+public class EventHandler extends Thread{
+
+    private LinkedList<Listener> listeners = new LinkedList<>();
+    private boolean isRunning = true;
+
+    @Override
+    public void run() {
+        //here the events are used to call the methods
+        while (isRunning){
+
+            if(EventQueue.getINSTANCE().getEvents().size() != 0){
+
+                //if there is an event in the event queue then it gets popped and the listeners get called
+                Event event = EventQueue.getINSTANCE().popEvent();
+
+                for(Listener listener : listeners){
+
+                    if(event instanceof MoveEvent){
+                        listener.onMove((MoveEvent) event);
+                    }
+
+                    //todo add event calling for rest of the events
+
+                }
+
+            }
+
+        }
+    }
+
+    public void addListener(Listener l){
+        listeners.add(l);
+    }
+}
