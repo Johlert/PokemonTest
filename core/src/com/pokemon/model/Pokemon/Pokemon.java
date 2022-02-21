cc740f48d1bdda566224589b354da970d6d01d77
Johlert <53093811+Johlert@users.noreply.github.com>
2022-02-21 16:42:17 +0100

    Create Pokemon.java

---
+++ core/src/com/pokemon/model/Pokemon/Pokemon.java
@@ -0,0 +1,23 @@
+package com.pokemon.model.Pokemon;
+
+import lombok.Data;
+
+public @Data class Pokemon {
+
+    private int level;
+    private int exp;
+    private String name;
+
+    private StatusEffect statusEffect;
+    private Ability[] abilities;
+    private PokemonType pokemonType;
+
+    //stats
+    private int hp;
+    private int attack;
+    private int defense;
+    private int specialAttack;
+    private int specialDefense;
+    private int speed;
+
+}
