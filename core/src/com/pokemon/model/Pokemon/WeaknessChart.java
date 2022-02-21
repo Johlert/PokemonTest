9e9ad03ef09a0d8721e4b0bc0e56d2c989ffb915
Johlert <53093811+Johlert@users.noreply.github.com>
2022-02-21 16:42:29 +0100

    Create WeaknessChart.java

---
+++ core/src/com/pokemon/model/Pokemon/WeaknessChart.java
@@ -0,0 +1,22 @@
+package com.pokemon.model.Pokemon;
+
+import lombok.Data;
+
+public @Data class WeaknessChart {
+
+    private String name;
+    //private HashMap<Type, Integer> chart;
+    private double[][] chart;
+
+    public WeaknessChart(){}
+
+    public double getDamageModifier(Type attack,Type[] pokemon){
+        if(pokemon.length == 1){
+            return chart[attack.getId()][pokemon[0].getId()];
+        }else{
+            //todo
+        }
+        return 1;
+    }
+
+}
