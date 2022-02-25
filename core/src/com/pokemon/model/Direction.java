df30d4dab8bdba70aa86cd6ac35a2772d8df67fb
Johlert <jan.ohlert@gmail.com>
2022-02-25 10:11:21 +0100

    Update Direction.java

--- core/src/com/pokemon/model/Direction.java
+++ core/src/com/pokemon/model/Direction.java
@@ -3,5 +3,21 @@ package com.pokemon.model;
 import java.io.Serializable;
 
 public enum Direction implements Serializable {
-    UP,LEFT, DOWN, RIGHT
+    UP(0, Global.TILE_SIZE), LEFT(-Global.TILE_SIZE, 0), DOWN(0, -Global.TILE_SIZE), RIGHT(Global.TILE_SIZE, 0);
+
+    private final int dx;
+    private final int dy;
+
+    Direction(int dx, int dy) {
+        this.dx = dx;
+        this.dy = dy;
+    }
+
+    public int getDx() {
+        return dx;
+    }
+
+    public int getDy() {
+        return dy;
+    }
 }
