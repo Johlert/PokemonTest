9364330e6870a1aa5d1d245df325ba1054a4d358
Johlert <jan.ohlert@gmail.com>
2022-02-25 10:11:25 +0100

    Update Player.java

--- core/src/com/pokemon/model/Player.java
+++ core/src/com/pokemon/model/Player.java
@@ -1,9 +1,12 @@
 package com.pokemon.model;
 
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.maps.objects.TextureMapObject;
 import com.badlogic.gdx.maps.tiled.TiledMap;
 import com.badlogic.gdx.maps.tiled.TiledMapTileLayer;
+import com.badlogic.gdx.math.Interpolation;
 import com.pokemon.controller.UserSettings;
+import com.pokemon.model.Events.EventQueue;
 import com.pokemon.model.Events.Listener;
 import com.pokemon.model.Events.MapJoinEvent;
 import com.pokemon.model.Events.MoveEvent;
@@ -12,6 +15,8 @@ import com.pokemon.model.Pokemon.Trainer;
 
 import java.io.Serializable;
 import java.util.LinkedList;
+
+import com.pokemon.view.utils.AnimationSet;
 import lombok.*;
 
 public @Data class Player implements Serializable, Trainer, Listener {
@@ -27,6 +32,23 @@ public @Data class Player implements Serializable, Trainer, Listener {
     private LinkedList<Flag> flags;
     private int speedInTilesPerSec = 2;
 
+    private int srcX, srcY, destX, destY;
+    private float curAnimDur;
+    private final float ANIM_DUR = 0.5f;
+    private ACTOR_STATE state;
+
+    private float curWalkDur;
+    private boolean moveRequestThisFrame;
+
+    private Direction facing;
+
+    private AnimationSet animationSet;
+
+    public enum ACTOR_STATE {
+        WALKING,
+        STANDING;
+    }
+
     public int getSpeedInTilesPerSec() {
         return speedInTilesPerSec;
     }
@@ -36,69 +58,116 @@ public @Data class Player implements Serializable, Trainer, Listener {
         return activePokemon;
     }
 
+    public void setAnimationSet(AnimationSet animationSet) {
+        this.animationSet = animationSet;
+    }
+
     public Player(TiledMap map, TextureMapObject tmo, int x, int y) {
         this.map = map;
         this.tmo = tmo;
         tmo.setX(x* Global.TILE_SIZE);
         tmo.setY(y* Global.TILE_SIZE);
+        state = ACTOR_STATE.STANDING;
+        facing = Direction.DOWN;
     }
 
-    public int getX() {
-        return (int) tmo.getX();
+    public float getX() {
+        return tmo.getX();
     }
 
-    public int getY() {
-        return (int) tmo.getY();
+    public float getY() {
+        return tmo.getY();
     }
 
-    public void move(int dx, int dy) {
-        float x = tmo.getX();
-        float y = tmo.getY();
+    public boolean move(Direction dir) {
+        int x = (int) tmo.getX();
+        int y = (int) tmo.getY();
+
+        if (state == ACTOR_STATE.WALKING) {
+            if (facing == dir) {
+                moveRequestThisFrame = true;
+            }
+            return false;
+        }
 
         TiledMapTileLayer layer = (TiledMapTileLayer) map.getLayers().get("Collision Layer");
 
-        if (layer.getCell((int) (x + dx) / Global.TILE_SIZE, (int) (y + dy) / Global.TILE_SIZE) == null) {
-            tmo.setX(x+dx);
-            tmo.setY(y+dy);
+        if (layer.getCell((x + dir.getDx()) / Global.TILE_SIZE, (y + dir.getDy()) / Global.TILE_SIZE) == null) {
+            initializeMove(x, y, dir);
+            tmo.setX(x+dir.getDx());
+            tmo.setY(y+dir.getDy());
         } else {
-            System.out.println(layer.getCell((int) (x + dx) / Global.TILE_SIZE, (int) (y + dy) / Global.TILE_SIZE).getTile().getProperties());
+            System.out.println(layer.getCell((x + dir.getDx()) / Global.TILE_SIZE, (y + dir.getDy()) / Global.TILE_SIZE).getTile().getProperties());
+            return false;
         }
+        return true;
+    }
+
+    private void initializeMove(int x1, int y1, Direction dir){
+        this.facing = dir;
+        srcX = x1;
+        srcY = y1;
+        destX = x1 + dir.getDx();
+        destY = y1 + dir.getDy();
+        curAnimDur = 0;
+        state = ACTOR_STATE.WALKING;
+    }
+
+    private void finishMove(){
+        state = ACTOR_STATE.STANDING;
+        tmo.setX(destX);
+        tmo.setY(destY);
+    }
+
+    public void update(float delta) {
+        if (state == ACTOR_STATE.WALKING){
+            curAnimDur += delta;
+            curWalkDur += delta;
+            tmo.setX(Interpolation.linear.apply(srcX, destX, curAnimDur/ANIM_DUR));
+            tmo.setY(Interpolation.linear.apply(srcY, destY, curAnimDur/ANIM_DUR));
+            if (curAnimDur>ANIM_DUR) {
+                float leftOverTime = curAnimDur-ANIM_DUR;
+                curWalkDur -= leftOverTime;
+                finishMove();
+                if (moveRequestThisFrame) {
+                    //MoveEvent mv = new MoveEvent(CacheForPoke.getInstance().getLocalP(), facing);
+                    //CacheForPoke.getInstance().getPostOffice().broadcast(mv);
+                    //EventQueue.getINSTANCE().addEvent(mv);
+                    move(facing);
+                } else {
+                    curWalkDur = 0f;
+                }
+            }
+        }
+        moveRequestThisFrame = false;
     }
 
     @Override
     public void onMove(MoveEvent moveEvent) {
         if(moveEvent.getP().equals(this)){
-            if(moveEvent.getDirection().equals(Direction.UP)){
-                move(0, Global.TILE_SIZE);
-            }
-            if(moveEvent.getDirection().equals(Direction.DOWN)){
-                move(0, -Global.TILE_SIZE);
-            }
-            if(moveEvent.getDirection().equals(Direction.LEFT)){
-                move(-Global.TILE_SIZE, 0);
-            }
-            if(moveEvent.getDirection().equals(Direction.RIGHT)){
-                move(Global.TILE_SIZE, 0);
-            }
-        }else {
-            if(moveEvent.getDirection().equals(Direction.UP)){
-                moveEvent.getP().move(0, Global.TILE_SIZE);
-            }
-            if(moveEvent.getDirection().equals(Direction.DOWN)){
-                moveEvent.getP().move(0, -Global.TILE_SIZE);
-            }
-            if(moveEvent.getDirection().equals(Direction.LEFT)){
-                moveEvent.getP().move(-Global.TILE_SIZE, 0);
-            }
-            if(moveEvent.getDirection().equals(Direction.RIGHT)){
-                moveEvent.getP().move(Global.TILE_SIZE, 0);
-            }
-
+            move(moveEvent.getDirection());
+        } else {
+            moveEvent.getP().move(moveEvent.getDirection());
         }
-
     }
 
     @Override
     public void onMapJoin(MapJoinEvent mapJoinEvent) {
+    }
+
+    String cur = "";
+    public TextureRegion getSprite(){
+        if (state == ACTOR_STATE.WALKING){
+            String temp = animationSet.getWalking(facing).getKeyFrame(curWalkDur).toString();
+            if (!temp.equals(cur)){
+                System.out.println(temp);
+                cur = temp;
+            }
+
+            return (TextureRegion) animationSet.getWalking(facing).getKeyFrame(curWalkDur);
+        } else if (state == ACTOR_STATE.STANDING) {
+            return animationSet.getStanding(facing);
+        }
+        return animationSet.getStanding(Direction.DOWN);
     }
 }
