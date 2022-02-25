646efe5b4cc1182e3e2871074a249bbf23ab2376
Johlert <jan.ohlert@gmail.com>
2022-02-25 10:11:18 +0100

    Update UserSettings.java

--- core/src/com/pokemon/controller/UserSettings.java
+++ core/src/com/pokemon/controller/UserSettings.java
@@ -5,15 +5,15 @@ import com.badlogic.gdx.Input;
 public class UserSettings {
 
     //todo the settings will be saved here the static settings are a placeholder!
-    private int moveForward = Input.Keys.UP;
+    private int moveUp = Input.Keys.UP;
     private int moveLeft = Input.Keys.LEFT;
     private int moveDown = Input.Keys.DOWN;
     private int moveRight = Input.Keys.RIGHT;
 
 
 
-    public int getMoveForward() {
-        return moveForward;
+    public int getMoveUp() {
+        return moveUp;
     }
 
     public int getMoveLeft() {
