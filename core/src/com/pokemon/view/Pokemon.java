4c7df5e552078fba5ec29593dc93b2c92ee243ab
Johlert <jan.ohlert@gmail.com>
2022-02-25 10:11:29 +0100

    Update Pokemon.java

--- core/src/com/pokemon/view/Pokemon.java
+++ core/src/com/pokemon/view/Pokemon.java
@@ -2,13 +2,19 @@ package com.pokemon.view;
 
 import com.badlogic.gdx.Game;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.assets.AssetManager;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
-import com.pokemon.view.screens.game.GameScreen;
+import com.badlogic.gdx.graphics.g2d.TextureAtlas;
 import com.pokemon.view.screens.menu.MainMenuScreen;
 
 public class Pokemon extends Game {
     private SpriteBatch batch;
+    private AssetManager assetManager;
+
+    public AssetManager getAssetManager() {
+        return assetManager;
+    }
 
     public SpriteBatch getBatch() {
         return batch;
@@ -17,6 +23,10 @@ public class Pokemon extends Game {
     @Override
     public void create() {
         batch = new SpriteBatch();
+        assetManager = new AssetManager();
+        assetManager.load("atlas/player_sprites.atlas", TextureAtlas.class);
+        assetManager.finishLoading();
+
         setScreen(new MainMenuScreen(this));
     }
 
