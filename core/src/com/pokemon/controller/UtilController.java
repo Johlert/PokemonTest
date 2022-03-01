9fd8fd564bb2beb75c49f688de0e126edff7a8fe
Johlert <53093811+Johlert@users.noreply.github.com>
2022-03-01 16:45:05 +0100

    Create UtilController.java

---
+++ core/src/com/pokemon/controller/UtilController.java
@@ -0,0 +1,37 @@
+package com.pokemon.controller;
+
+import com.badlogic.gdx.Input;
+import com.badlogic.gdx.InputAdapter;
+import com.pokemon.view.screens.game.GameScreen;
+
+public class UtilController extends InputAdapter {
+    private final GameScreen gamescreen;
+    private boolean collisionVisible = false;
+
+    public UtilController(GameScreen gameScreen) {
+        this.gamescreen = gameScreen;
+    }
+
+    @Override
+    public boolean keyDown(int keycode) {
+        if (keycode == Input.Keys.SPACE) {
+            gamescreen.getPlayer().setANIM_DUR(0.1f);
+        }
+
+        return false;
+    }
+
+    @Override
+    public boolean keyUp(int keycode) {
+        if (keycode == Input.Keys.SPACE) {
+            gamescreen.getPlayer().setANIM_DUR(0.5f);
+        }
+
+        if (keycode == Input.Keys.F12) {
+            collisionVisible = !collisionVisible;
+            gamescreen.getMap().getLayers().get("Collision Layer").setVisible(collisionVisible);
+        }
+
+        return false;
+    }
+}
