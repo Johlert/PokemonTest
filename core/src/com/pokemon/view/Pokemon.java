1dcabc7d78b45188591d5cd4732b6f5df016e871
Johlert <53093811+Johlert@users.noreply.github.com>
2022-03-01 16:45:28 +0100

    Update Pokemon.java

--- core/src/com/pokemon/view/Pokemon.java
+++ core/src/com/pokemon/view/Pokemon.java
@@ -3,14 +3,18 @@ package com.pokemon.view;
 import com.badlogic.gdx.Game;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.assets.AssetManager;
+import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.GL20;
-import com.badlogic.gdx.graphics.g2d.SpriteBatch;
-import com.badlogic.gdx.graphics.g2d.TextureAtlas;
+import com.badlogic.gdx.graphics.g2d.*;
+import com.badlogic.gdx.graphics.g2d.freetype.FreeTypeFontGenerator;
+import com.badlogic.gdx.scenes.scene2d.ui.Label;
+import com.badlogic.gdx.scenes.scene2d.ui.Skin;
 import com.pokemon.view.screens.menu.MainMenuScreen;
 
 public class Pokemon extends Game {
     private SpriteBatch batch;
     private AssetManager assetManager;
+    private Skin skin;
 
     public AssetManager getAssetManager() {
         return assetManager;
@@ -25,9 +29,49 @@ public class Pokemon extends Game {
         batch = new SpriteBatch();
         assetManager = new AssetManager();
         assetManager.load("atlas/player_sprites.atlas", TextureAtlas.class);
+        assetManager.load("atlas/ui.atlas", TextureAtlas.class);
+        assetManager.load("fonts/small_letters_font.fnt", BitmapFont.class);
         assetManager.finishLoading();
 
+        initSkin();
+
         setScreen(new MainMenuScreen(this));
+    }
+
+    private void initSkin() {
+        skin = new Skin();
+        TextureAtlas uiAtlas = assetManager.get("atlas/ui.atlas");
+
+        NinePatch dialogueBox = new NinePatch(uiAtlas.findRegion("dialogueBox"), 7, 7, 6, 6);
+        skin.add("dialogueBox", dialogueBox);
+        NinePatch optionsBox = new NinePatch(uiAtlas.findRegion("optionsBox"), 12, 12, 5, 5);
+        skin.add("optionsBox", optionsBox);
+        skin.add("greyArrow", uiAtlas.findRegion("greyArrow"), TextureRegion.class);
+
+        FreeTypeFontGenerator generator = new FreeTypeFontGenerator(Gdx.files.internal("fonts/pkmnrsi.ttf"));
+        FreeTypeFontGenerator.FreeTypeFontParameter parameter = new FreeTypeFontGenerator.FreeTypeFontParameter();
+        parameter.size = 12;
+        parameter.color = new Color(248f / 255f, 248f / 255f, 248f / 255f, 1f);
+        parameter.shadowColor = new Color(96f / 255f, 96f / 255f, 96f / 255f, 1f);
+        parameter.shadowOffsetX = 1;
+        parameter.shadowOffsetY = 1;
+        parameter.characters = "!  \"  #  $  %  &  '  (  )  *  +  ,  -  .  /  0  1  2  3  4  5  6  7  8  9  :  ;  <  =  >  ?  @  A  B  C  D  E  F  G  H  I  J  K  L  M  N  O  P  Q  R  S  T  U  V  W  X  Y  Z  [  \\  ]  ^  _  `  a  b  c  d  e  f  g  h  i  j  k  l  m  n  o  p  q  r  s  t  u  v  w  x  y  z  {  |  }  ~  \u2190  \u2191  \u2192  \u2193  \u2640  \u2642";
+
+        BitmapFont font = generator.generateFont(parameter);
+        generator.dispose();
+        font.getData().setLineHeight(16f);
+        skin.add("font", font);
+
+        BitmapFont smallFont = assetManager.get("fonts/small_letters_font.fnt", BitmapFont.class);
+        skin.add("small_letters_font", smallFont);
+
+        Label.LabelStyle labelStyle = new Label.LabelStyle();
+        labelStyle.font = skin.getFont("font");
+        skin.add("default", labelStyle);
+
+        Label.LabelStyle labelStyleSmall = new Label.LabelStyle();
+        labelStyleSmall.font = skin.getFont("small_letters_font");
+        skin.add("smallLabel", labelStyleSmall);
     }
 
     @Override
@@ -41,5 +85,9 @@ public class Pokemon extends Game {
     @Override
     public void dispose() {
         super.dispose();
+    }
+
+    public Skin getSkin() {
+        return skin;
     }
 }
