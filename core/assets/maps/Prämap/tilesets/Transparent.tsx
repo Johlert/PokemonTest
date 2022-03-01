8fc7312d189ee874fd5b26928a553109db58075a
Johlert <53093811+Johlert@users.noreply.github.com>
2022-03-01 16:44:46 +0100

    Update Transparent.tsx

--- core/assets/maps/Prämap/tilesets/Transparent.tsx
+++ core/assets/maps/Prämap/tilesets/Transparent.tsx
@@ -1,7 +1,10 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.8" tiledversion="1.7.91" name="Transparent" tilewidth="16" tileheight="16" tilecount="10" columns="5">
+<tileset version="1.8" tiledversion="1.8.1" name="Transparent" tilewidth="16" tileheight="16" tilecount="10" columns="5">
  <image source="imgs/Transparent Colors.png" width="80" height="32"/>
  <tile id="0">
+  <properties>
+   <property name="collision" type="bool" value="true"/>
+  </properties>
   <objectgroup draworder="index" id="2">
    <object id="1" x="0" y="0" visible="0">
     <polyline points="0,0 16,0 16,16 0,16 0,0"/>
@@ -10,5 +13,50 @@
     <polyline points="0,0 16,0 16,16 0,16 0,0"/>
    </object>
   </objectgroup>
+ </tile>
+ <tile id="1">
+  <properties>
+   <property name="collision" type="bool" value="false"/>
+  </properties>
+ </tile>
+ <tile id="2">
+  <properties>
+   <property name="collision" type="bool" value="false"/>
+  </properties>
+ </tile>
+ <tile id="3">
+  <properties>
+   <property name="collision" type="bool" value="false"/>
+  </properties>
+ </tile>
+ <tile id="4">
+  <properties>
+   <property name="collision" type="bool" value="false"/>
+  </properties>
+ </tile>
+ <tile id="5">
+  <properties>
+   <property name="collision" type="bool" value="false"/>
+  </properties>
+ </tile>
+ <tile id="6">
+  <properties>
+   <property name="collision" type="bool" value="false"/>
+  </properties>
+ </tile>
+ <tile id="7">
+  <properties>
+   <property name="collision" type="bool" value="false"/>
+  </properties>
+ </tile>
+ <tile id="8">
+  <properties>
+   <property name="collision" type="bool" value="false"/>
+  </properties>
+ </tile>
+ <tile id="9">
+  <properties>
+   <property name="collision" type="bool" value="false"/>
+  </properties>
  </tile>
 </tileset>
