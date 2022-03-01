b7dad362570b12b5c31acb15fe40d35190657bf8
Johlert <53093811+Johlert@users.noreply.github.com>
2022-03-01 16:45:01 +0100

    Update UserSettings.java

--- core/src/com/pokemon/controller/UserSettings.java
+++ core/src/com/pokemon/controller/UserSettings.java
@@ -1,30 +1,14 @@
 package com.pokemon.controller;
 
 import com.badlogic.gdx.Input;
+import lombok.Data;
 
-public class UserSettings {
-
-    //todo the settings will be saved here the static settings are a placeholder!
+public @Data
+class UserSettings {
     private int moveUp = Input.Keys.UP;
     private int moveLeft = Input.Keys.LEFT;
     private int moveDown = Input.Keys.DOWN;
     private int moveRight = Input.Keys.RIGHT;
-
-
-
-    public int getMoveUp() {
-        return moveUp;
-    }
-
-    public int getMoveLeft() {
-        return moveLeft;
-    }
-
-    public int getMoveDown() {
-        return moveDown;
-    }
-
-    public int getMoveRight() {
-        return moveRight;
-    }
+    private int a = Input.Keys.Y;
+    private int b = Input.Keys.X;
 }
