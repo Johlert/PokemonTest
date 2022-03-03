142237c970842d71f4960ab398c2a56f9cc9a97e
Gogolinolett <50708111+Gogolinolett@users.noreply.github.com>
2022-03-03 20:05:17 +0100

    Update MapJoinEvent.java

--- core/src/com/pokemon/model/Events/MapJoinEvent.java
+++ core/src/com/pokemon/model/Events/MapJoinEvent.java
@@ -2,13 +2,19 @@ package com.pokemon.model.Events;
 
 import com.pokemon.model.Map;
 import com.pokemon.model.Player;
+import com.pokemon.model.Position;
 import lombok.Data;
 
 public @Data
 class MapJoinEvent implements Event{
 
-    private Map map;
+    public MapJoinEvent(String name, Position position){
+        this.name = name;
+        this.position = position;
+    }
+
     private String name;
+    private Position position;
 
     @Override
     public EventType getType() {
