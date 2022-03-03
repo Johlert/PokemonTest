b0794ed78f42bed8a89de5b50303c0dddbe46d88
Gogolinolett <50708111+Gogolinolett@users.noreply.github.com>
2022-03-03 20:06:26 +0100

    Update Position.java

--- core/src/com/pokemon/model/Position.java
+++ core/src/com/pokemon/model/Position.java
@@ -1,9 +1,17 @@
 package com.pokemon.model;
 
+import lombok.Data;
+
 import java.io.Serializable;
 
-public class Position implements Serializable {
+public @Data class Position implements Serializable {
     //this a data class in which
+
+
+    public Position(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
 
     private int x;
     private int y;
