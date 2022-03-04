b1dd2679cd3aa98efdc40475e309411afa3a4fd9
Gogolinolett <50708111+Gogolinolett@users.noreply.github.com>
2022-03-04 10:24:12 +0100

    Update MoveEvent.java

--- core/src/com/pokemon/model/Events/MoveEvent.java
+++ core/src/com/pokemon/model/Events/MoveEvent.java
@@ -3,12 +3,14 @@ package com.pokemon.model.Events;
 
 import com.pokemon.model.Direction;
 import com.pokemon.model.Player;
+import com.pokemon.model.Position;
 import lombok.Data;
 
 public @Data class MoveEvent implements Event{
 
     private String name;
     private Direction direction;
+    private Position pos;
 
 
     @Override
@@ -16,9 +18,10 @@ public @Data class MoveEvent implements Event{
         return EventType.PLAYERMOVEEVENT;
     }
 
-    public MoveEvent(String name, Direction direction) {
+    public MoveEvent(String name, Direction direction, Position position) {
         this.name = name;
         this.direction = direction;
+        pos = position;
     }
 
 
