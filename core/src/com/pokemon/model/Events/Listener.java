83442ba2322e021dc828c0555d5c70d19762ee7e
Gogolinolett <50708111+Gogolinolett@users.noreply.github.com>
2022-03-04 22:36:52 +0100

    Update Listener.java

--- core/src/com/pokemon/model/Events/Listener.java
+++ core/src/com/pokemon/model/Events/Listener.java
@@ -3,5 +3,5 @@ package com.pokemon.model.Events;
 public interface Listener {
     void onMove(MoveEvent moveEvent);
     void onMapJoin(MapJoinEvent mapJoinEvent);
-    //todo add all events int the listener
+    void onPlayerFacing(FacingEvent facingEvent);
 }
