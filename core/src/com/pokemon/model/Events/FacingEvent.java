21d368e40b96d2a1a11b5f0f781dadadd36b8cfa
Gogolinolett <50708111+Gogolinolett@users.noreply.github.com>
2022-03-04 22:36:46 +0100

    Update FacingEvent.java

--- core/src/com/pokemon/model/Events/FacingEvent.java
+++ core/src/com/pokemon/model/Events/FacingEvent.java
@@ -1,8 +1,20 @@
 package com.pokemon.model.Events;
 
-public class FacingEvent implements Event{
+import com.pokemon.model.Direction;
+import lombok.Data;
+
+public @Data class FacingEvent implements Event{
+
+    private Direction direction;
+    private String name;
+
+    public FacingEvent(Direction direction, String name) {
+        this.direction = direction;
+        this.name = name;
+    }
+
     @Override
     public EventType getType() {
-        return null;
+        return EventType.PLAYERFACINGEVENT;
     }
 }
