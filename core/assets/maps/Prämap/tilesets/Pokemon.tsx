9805e0dce69fce43f984ce332d5a65124dac65a9
Johlert <53093811+Johlert@users.noreply.github.com>
2022-03-04 22:42:44 +0100

    Create Pokemon.tsx

---
+++ core/assets/maps/Prämap/tilesets/Pokemon.tsx
@@ -0,0 +1,23 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.8" tiledversion="1.8.1" name="Pokemon" tilewidth="16" tileheight="16" tilecount="1767" columns="31">
+ <image source="../tilesets/imgs/pokemon.png" trans="000000" width="496" height="912"/>
+ <tile id="1032">
+  <animation>
+   <frame tileid="1062" duration="1000"/>
+   <frame tileid="1061" duration="1000"/>
+   <frame tileid="1030" duration="1000"/>
+   <frame tileid="1062" duration="1000"/>
+   <frame tileid="1031" duration="1000"/>
+  </animation>
+ </tile>
+ <tile id="1033">
+  <animation>
+   <frame tileid="1030" duration="700"/>
+   <frame tileid="1061" duration="700"/>
+   <frame tileid="1031" duration="700"/>
+   <frame tileid="1062" duration="700"/>
+   <frame tileid="1031" duration="700"/>
+   <frame tileid="1061" duration="700"/>
+  </animation>
+ </tile>
+</tileset>
