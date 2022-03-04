ac2ad1f4ee7feb1f57c5672b6310c78a96d336a5
Johlert <53093811+Johlert@users.noreply.github.com>
2022-03-04 22:42:49 +0100

    Create NPCs2.tsx

---
+++ core/assets/maps/Prämap/tilesets/NPCs2.tsx
@@ -0,0 +1,20 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.8" tiledversion="1.8.1" name="NPCs2" tilewidth="16" tileheight="16" tilecount="1850" columns="50">
+ <image source="../tilesets/imgs/tileset-hoenn_ows_remake2.png" width="800" height="600"/>
+ <tile id="1309">
+  <animation>
+   <frame tileid="1410" duration="2500"/>
+   <frame tileid="1308" duration="2500"/>
+   <frame tileid="1310" duration="2500"/>
+   <frame tileid="1308" duration="2500"/>
+  </animation>
+ </tile>
+ <tile id="1359">
+  <animation>
+   <frame tileid="1460" duration="2500"/>
+   <frame tileid="1358" duration="2500"/>
+   <frame tileid="1360" duration="2500"/>
+   <frame tileid="1358" duration="2500"/>
+  </animation>
+ </tile>
+</tileset>
